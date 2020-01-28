2b9220d05bcb6f27a7899af748db578b92832edf
ychiodi <parkietje@wlan-145-94-178-146.wlan.tudelft.nl>
2020-01-29 00:56:04 +0100

    update run script to generate example config

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #works on UNIX
-cd $PWD/cmd
+cd $PWD
+[ -f config.json ] || printf '{\n"authorization": {\n\t"username" : "example@email.com",\n\t"password" : "yourPassword",\n\t"server" : "smtp-relay.gmail.com:587"\n\t}\n}' > test.json
+
+cd cmd
 go build -o $PWD/cmd
 echo "build succes!"
 echo "start execution"
